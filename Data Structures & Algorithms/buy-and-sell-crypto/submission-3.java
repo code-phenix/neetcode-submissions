22c08d3558507ce7392a5e66784906242ec271a0
codepheonix <55018311+code-phenix@users.noreply.github.com>
2026-08-11 22:49:57 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    // [10,1,5,6,7,1]
+    //      i,
+    //      6
+    
+
+    public int maxProfit(int[] prices) {
+        int maxProfit=0;
+        int minPrice=Integer.MAX_VALUE;
+        for(int price:prices){
+            minPrice=Math.min(minPrice,price);
+            maxProfit=Math.max(maxProfit,price-minPrice);
+        }
+        return maxProfit;
+       
+        
+    }
+}
